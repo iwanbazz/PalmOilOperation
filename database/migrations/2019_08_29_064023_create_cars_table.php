e0623a10b9241492cb3d0ae10286c159f818448b
iwanbazz <iwan.bazz@gmail.com>
2019-08-29 16:23:33 +0700

    Added migration table cars

---
+++ database/migrations/2019_08_29_064023_create_cars_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('carname');
+            $table->string('cartype');
+            $table->string('carplate')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
